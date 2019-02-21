495eceebfdd8d02c9fa1688d70894e7caae18e43
Marian Czerniejewski <marian@czerniejewski.pl>
2019-02-21 21:45:31 +0100

    test create loan

---
+++ tests/Lendinvest/Loan/LoanTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Lendinvest\Loan;
+
+use PHPUnit\Framework\Assert;
+use PHPUnit\Framework\TestCase;
+
+class LoanTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function when_data_is_correct_then_loan_can_be_create()
+    {
+        $loanId = LoanId::fromString('1');
+        $loan = Loan::create(
+            $loanId,
+            new \DateTimeImmutable(' 2015-10-1'),
+            new \DateTimeImmutable('2015-11-15')
+        );
+        Assert::assertEquals($loanId, $loan->id());
+    }
+}
